2ff85b5bf56776cce4f4dd209093eb09d4590c09
luanmvcosta-engeman <luan.vieira@engeman.net>
2026-07-30 23:28:08 -0300

    feat: create UserEntity class and attributes

---
+++ src/main/java/com/barberflow/be/modules/user/core/entity/UserEntity.java
@@ -0,0 +1,15 @@
+package com.barberflow.be.modules.user.core.entity;
+
+import java.time.LocalDateTime;
+
+public class UserEntity {
+
+  private Long id;
+  private String cpf;
+  private String nome;
+  private String email;
+  private String password;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+  
+}
