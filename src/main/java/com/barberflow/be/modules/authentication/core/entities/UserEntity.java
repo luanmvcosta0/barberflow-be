560e3c3f8520bdf9a4f71e14686f00a090d32cc2
luanmvcosta0 <luanmvcosta@gmail.com>
2026-08-18 13:02:40 -0300

    feat: add UserEntity class for user authentication management

---
+++ src/main/java/com/barberflow/be/modules/authentication/core/entities/UserEntity.java
@@ -0,0 +1,49 @@
+package com.barberflow.be.modules.authentication.core.entities;
+
+import java.time.LocalDateTime;
+
+public class UserEntity {
+
+  private Long id;
+  private String fullName;
+  private String email;
+  private String password;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+
+  public UserEntity() {}
+
+  public UserEntity(Long id, String fullName, String email, String password, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    this.id = id;
+    this.fullName = fullName;
+    this.email = email;
+    this.password = password;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
+  
+}
