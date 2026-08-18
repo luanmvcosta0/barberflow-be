b1201502f327029b06739aa5dee8ef02b3dee4f9
luanmvcosta0 <luanmvcosta@gmail.com>
2026-08-18 13:02:58 -0300

    feat: add UserGateway interface for user management operations

---
+++ src/main/java/com/barberflow/be/modules/authentication/core/gateway/UserGateway.java
@@ -0,0 +1,12 @@
+package com.barberflow.be.modules.authentication.core.gateway;
+
+import com.barberflow.be.modules.authentication.core.entities.UserEntity;
+
+public interface UserGateway {
+  
+  UserEntity createUser(UserEntity user);
+  UserEntity findUserById(Long id);
+  UserEntity updateUser(UserEntity user);
+  UserEntity deleteUser(Long id);
+
+}
